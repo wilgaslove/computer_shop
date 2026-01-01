9ec86b5cb3ac37f5e41c0fbff8bed83db0633485
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-01-01 04:10:25 +0100

    dossiers layouts

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <header style="padding: 1rem; background: #111; color: white;">
+      <h2>🖥️ Computer Shop</h2>
+    </header>
+
+    <main style="padding: 2rem;">
+      <slot />
+    </main>
+  </div>
+</template>
