514918d5176af839bda757693edc3f68d7fe4f48
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-01-01 03:56:48 +0100

    installation de npm install -D @vitejs/plugin-vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <title>Computer Shop</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
