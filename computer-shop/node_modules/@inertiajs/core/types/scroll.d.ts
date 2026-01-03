b08492de9a893f3c994d80fc502c6b7b1125e937
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-03 17:23:00 +0100

    sans projet

---
+++ computer-shop/node_modules/@inertiajs/core/types/scroll.d.ts
@@ -0,0 +1,13 @@
+import { ScrollRegion } from './types';
+export declare class Scroll {
+    static save(): void;
+    static getScrollRegions(): ScrollRegion[];
+    protected static regions(): NodeListOf<Element>;
+    static reset(): void;
+    static scrollToAnchor(): void;
+    static restore(scrollRegions: ScrollRegion[]): void;
+    static restoreScrollRegions(scrollRegions: ScrollRegion[]): void;
+    static restoreDocument(): void;
+    static onScroll(event: Event): void;
+    static onWindowScroll(): void;
+}
