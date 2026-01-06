b47e099fa91a2d313bbbad11db4d195bc1c25fa9
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-06 20:58:23 +0100

    model catégory et product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name'];
-
-    public function products()
-    {
-        return $this->hasMany(Product::class);
-    }
+    //
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,17 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = [
-        'name',
-        'description',
-        'price',
-        'stock',
-        'image',
-        'category_id',
-    ];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
+    //
 }
