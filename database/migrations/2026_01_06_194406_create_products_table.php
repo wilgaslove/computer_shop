062932b071039860b62f277bef10843512ab9dbe
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-06 20:57:36 +0100

    model catégory et product

--- database/migrations/2026_01_06_194406_create_products_table.php
+++ database/migrations/2026_01_06_194406_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->string('image')->nullable();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
