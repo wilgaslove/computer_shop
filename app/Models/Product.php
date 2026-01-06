d5ceba3dae4cd92ccff637893d247cf5515e2b6d
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-06 21:34:03 +0100

    BDD CREEE, PRODUCTCONTROLE

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
