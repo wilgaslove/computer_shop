94693158a7ae24a9d23451102b8bc2d9392505cd
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-07 21:43:23 +0100

    dashboard admin

---
+++ resources/js/Layouts/Admin/Dashboard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+
+defineProps({ stats: Object })
+</script>
+
+<template>
+  <AdminLayout>
+    <h1 class="text-2xl font-bold mb-6">Dashboard Admin</h1>
+
+    <div class="grid grid-cols-2 gap-6">
+      <div class="bg-white p-4 shadow rounded">
+        <p class="text-gray-500">Produits</p>
+        <p class="text-3xl font-bold">{{ stats.products }}</p>
+      </div>
+
+      <div class="bg-white p-4 shadow rounded">
+        <p class="text-gray-500">Catégories</p>
+        <p class="text-3xl font-bold">{{ stats.categories }}</p>
+      </div>
+    </div>
+  </AdminLayout>
+</template>
