7763ac277e39c722a396e6019576316631791fb0
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-07 20:41:10 +0100

    categorycontroller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,10 +1,57 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Category;
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Categories/Index', [
+            'categories' => Category::all(),
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Categories/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+        Category::create($request->only('name'));
+
+        return redirect()->route('categories.index');
+    }
+
+    public function edit(Category $category)
+    {
+        return Inertia::render('Categories/Edit', [
+            'category' => $category,
+        ]);
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+        $category->update($request->only('name'));
+
+        return redirect()->route('categories.index');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index');
+    }
 }
