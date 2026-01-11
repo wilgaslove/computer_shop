1ddf8cdd321c92cd03a3c3ca5a4cf9ac91f6dd69
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-11 08:55:25 +0100

    RolePermissionSeeder

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -5,13 +5,24 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 class RolePermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        // Permissions
+        Permission::create(['name' => 'manage categories']);
+        Permission::create(['name' => 'manage products']);
+        Permission::create(['name' => 'view dashboard']);
+
+        // Roles
+        $admin = Role::create(['name' => 'admin']);
+        $user  = Role::create(['name' => 'user']);
+
+        // Attribution permissions
+        $admin->givePermissionTo(Permission::all());
+        $user->givePermissionTo('view dashboard');
     }
 }
