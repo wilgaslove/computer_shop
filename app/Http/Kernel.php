edeebf941589cc959a5a742099d3407f55ca6469
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-17 18:24:18 +0100

    suppression de rolemiddleware.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,13 +1,1 @@
-<?php
-
-namespace App\Http;
-
-use Illuminate\Foundation\Http\Kernel as HttpKernel;
-
-class Kernel extends HttpKernel
-{
-    protected $routeMiddleware = [
-        // ...
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
-    ];
-}
+<?php   
