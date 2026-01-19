576ec048256403aad9077d432a462cc03d846bf7
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-19 21:38:29 +0100

    chngement

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,1 +1,15 @@
-<?php   
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middlewareAliases = [
+    'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+    'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+    'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+];
+
+}
