a2639fe7e78ef7335629fbff6971555e3539e9d6
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-22 21:42:54 +0100

    cread category

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -17,10 +17,27 @@ class RolePermissionSeeder extends Seeder
         Permission::create(['name' => 'manage products']);
         Permission::create(['name' => 'view dashboard']);
 
+
+        Permission::create(['name' => 'category.view']);
+        Permission::create(['name' => 'category.create']);
+        Permission::create(['name' => 'category.edit']);
+        Permission::create(['name' => 'category.delete']);
+
+
+       
         // Roles
         $admin = Role::create(['name' => 'admin']);
         $user  = Role::create(['name' => 'user']);
 
+        
+        $admin->givePermissionTo([
+            'category.view',
+            'category.create',
+            'category.edit',
+            'category.delete',
+        ]);
+
+        
         // Attribution permissions
         $admin->givePermissionTo(Permission::all());
         $user->givePermissionTo('view dashboard');
