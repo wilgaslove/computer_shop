866a08a89b836b5f87ef7fa160ff450affd541e4
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-27 16:43:50 +0100

    ça marche avec Admin\prducts

--- resources/js/pages/Admin/Products/Index.vue
+++ resources/js/pages/Admin/Products/Index.vue
@@ -1,8 +1,69 @@
-<script>
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+  products: {
+    type: Array,
+    required: true,
+  },
+  can: {
+    type: Object,
+    default: () => ({
+      create: true,
+      edit: true,
+      delete: true,
+    }),
+  },
+})
+
 </script>
+
+
 <template>
   <div>
-    <h1 class="text-xl font-bold mb-4">Liste des produits</h1>
-    <!-- Contenu de la page des produits -->
+    <pre>{{ can }} </pre>
+    <h1 class="text-2xl font-bold mb-4">Produits</h1>
+
+    <Link
+      v-if="can.create"
+      :href="route('admin.products.create')"
+      class="btn btn-primary mb-4"
+    >
+      + Nouveau produit
+    </Link>
+
+    <table class="w-full border">
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Prix</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="product in products.data" :key="product.id">
+          <td>{{ product.name }}</td>
+          <td>{{ product.price }}F CFA</td> 
+          <td class="space-x-2">
+            <Link
+              v-if="can.edit"
+              :href="route('admin.products.edit', product.id)"
+            >
+              ✏️
+            </Link>
+
+            <Link
+              v-if="can.delete"
+              method="delete"
+              as="button"
+              :href="route('admin.products.destroy', product.id)"
+            >
+              🗑️
+            </Link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
