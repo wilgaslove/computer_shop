ae8cb48aa25cd698ed6824b6dcbd95343a0a0bbb
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-27 21:02:18 +0100

    changement achitectural

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,29 @@
 <?php
 
 use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// page d'accueil → produits
-Route::get('/', function () {
-    return redirect()->route('products.index');
-});
+// Admin
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware(['auth', 'role:admin'])
+    ->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])
+            ->name('dashboard');
 
+        Route::resource('products', Admin\ProductController::class);
+        Route::resource('categories', Admin\CategoryController::class);
+        // Route::resource('orders', Admin\OrderController::class);
+    });
 
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', [DashboardController::class, 'index'])
-        ->name('admin.dashboard');
+// Front (clients)
+// Route::get('/', [Shop\ProductController::class, 'home'])->name('shop.home');
+// Route::get('/products', [Shop\ProductController::class, 'index'])->name('shop.products');
+// Route::get('/products/{product}', [Shop\ProductController::class, 'show'])->name('shop.products.show');
 
-    Route::resource('categories', CategoryController::class);
-    Route::resource('products', ProductController::class);
-});
-
-
-// // produits
-Route::middleware(['auth'])->group(function () {
-    Route::resource('products', ProductController::class);
-    Route::resource('categories', CategoryController::class);
-});
-
-
-
-// Route::get('/dashboard', function () {
-//     return redirect()->route('admin.dashboard');
-// })->middleware(['auth'])->name('dashboard');
-
-
-// dashboard utilisateur
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-// profile
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
