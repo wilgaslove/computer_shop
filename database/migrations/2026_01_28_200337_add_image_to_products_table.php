7bf023fe4125849b0a040f88602bf04d423b2394
Wilfred ADJOVI <131034582+wilgaslove@users.noreply.github.com>
2026-01-28 21:16:39 +0100

    upload image

--- database/migrations/2026_01_28_200337_add_image_to_products_table.php
+++ database/migrations/2026_01_28_200337_add_image_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('image')->nullable()->after('price');
         });
     }
 
@@ -22,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
-            
+            $table->dropColumn('image');
         });
     }
 };
