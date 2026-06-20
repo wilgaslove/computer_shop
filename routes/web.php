f29d6ec7b2bf8f872e913643467fc1594c96854e
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-06-20 17:32:11 +0200

    router

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,29 @@
 <?php
 
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductController as AdminProductController;
 use App\Http\Controllers\Shop\ProductController as ShopProductController;
 
+/*
+|--------------------------------------------------------------------------
+| Boutique (publique)
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', [ShopProductController::class, 'index'])
+    ->name('home');
+
+Route::get('/shop', [ShopProductController::class, 'index'])
+    ->name('shop.products');
+
+
+/*
+|--------------------------------------------------------------------------
+| Administration
+|--------------------------------------------------------------------------
+*/
 
 Route::middleware(['auth', 'role:admin'])
     ->prefix('admin')
@@ -19,14 +33,30 @@ Route::middleware(['auth', 'role:admin'])
         Route::get('/dashboard', [DashboardController::class, 'index'])
             ->name('dashboard');
 
-        Route::resource('products', \App\Http\Controllers\Admin\ProductController::class);
-         Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
+        Route::resource('products', AdminProductController::class);
+
+        // Plus tard
+        // Route::resource('categories', CategoryController::class);
+        // Route::resource('orders', OrderController::class);
+        // Route::resource('users', UserController::class);
     });
 
 
-Route::get('/shop', [ShopProductController::class, 'index'])
-    ->name('shop.products');
+/*
+|--------------------------------------------------------------------------
+| Profil utilisateur
+|--------------------------------------------------------------------------
+*/
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
 
-    
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+});
+
 require __DIR__.'/auth.php';
