7f0c36df6f1781e9f0c30bc3bdfe0d64ab0c3661
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-07-02 12:45:26 +0200

    hero dynamique migration

--- database/migrations/2026_07_02_104132_create_hero_sliders_table.php
+++ database/migrations/2026_07_02_104132_create_hero_sliders_table.php
@@ -6,20 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('hero_sliders', function (Blueprint $table) {
+
             $table->id();
+
+            $table->string('title');
+
+            $table->string('subtitle')->nullable();
+
+            $table->string('button_text')->nullable();
+
+            $table->string('button_link')->nullable();
+
+            $table->string('image');
+
+            $table->integer('position')->default(0);
+
+            $table->boolean('active')->default(true);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('hero_sliders');
