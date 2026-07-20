9b9595acc52910184a5845bcdad7f582650df846
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-07-20 21:19:31 +0200

    correction

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,17 @@ Enregistremtn et Connexion  utilisateur
 |-------------------------------------------------------------------------- 
 */
 
-// Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-// Route::post('/login', [AuthController::class, 'login']);
+ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+ Route::post('/login', [AuthController::class, 'login']);
 
-// Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-// Route::post('/register', [AuthController::class, 'register']);
+ Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+ Route::post('/register', [AuthController::class, 'register']);
 
-// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
-// Route::get('/dashboard', function () {
-//     return \Inertia\Inertia::render('Dashboard');
-// })->middleware('auth');
+ Route::get('/dashboard', function () {
+     return \Inertia\Inertia::render('Dashboard');
+ })->middleware('auth');
 
 Route::middleware('guest')->group(function () {
 
