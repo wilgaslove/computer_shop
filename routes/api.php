0edd1da93e48409bc16dda776cf846a459c1152b
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-08-27 18:04:55 +0200

    login

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,22 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Auth\RegisteredUserController;
+// use App\Http\Controllers\Auth\AuthenticatedSessionController;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return response()->json([
-        'success' => true,
-        'message' => 'API Laravel fonctionne correctement.',
-    ]);
-});
+// Route::get('/test', function () {
+//     return response()->json([
+//         'success' => true,
+//         'message' => 'API Laravel fonctionne correctement.',
+//     ]);
+// });
 
-Route::post('/register', [
-    RegisteredUserController::class,
-    'apiRegister'
-]);
+// Route::post('/register', [
+//     RegisteredUserController::class,
+//     'apiRegister'
+// ]);
 
-Route::post('/login', [
-    AuthenticatedSessionController::class,
-    'apiLogin'
-]);
+// Route::post('/login', [
+//     AuthenticatedSessionController::class,
+//     'apiLogin'
+// ]);
